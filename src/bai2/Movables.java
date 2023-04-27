645e9d81d08b3c2c02f70fa2dbb85733c24cb99d
Phan Huy Hiệu <114344641+phanhuyhieu2004@users.noreply.github.com>
2023-04-27 14:45:32 +0700

    oop2

---
+++ src/bai2/Movables.java
@@ -0,0 +1,82 @@
+package bai2;
+
+public interface Movables {
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+}
+
+ class MovablePoint implements Movable {
+    private int x;
+    private int y;
+    private int xSpeed;
+    private int ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + "), speed=(" + xSpeed + "," + ySpeed + ")";
+    }
+
+    @Override
+    public void moveUp() {
+        y -= ySpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        y += ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        x -= xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        x += xSpeed;
+    }
+}
+
+ class MovableCircle implements Movable {
+    private int radius;
+    private MovablePoint center;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return center.toString() + ", radius=" + radius;
+    }
+
+    @Override
+    public void moveUp() {
+        center.moveUp();
+    }
+
+    @Override
+    public void moveDown() {
+        center.moveDown();
+    }
+
+    @Override
+    public void moveLeft() {
+        center.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        center.moveRight();
+    }
+}
