359c8b7c470cb73bddc33b4aeaa19187a5feb72d
Phan Huy Hiệu <114344641+phanhuyhieu2004@users.noreply.github.com>
2023-05-17 09:07:16 +0700

    bt 5

--- src/bai5/Product.java
+++ src/bai5/Product.java
@@ -8,7 +8,7 @@ public class Product implements Serializable {
     private String proName;
     private double price;
 
-    public Product() {
+    public Product(String p001, String s, int i, String dell) {
     }
 
     public Product(String proID, String proName, double price) {
@@ -43,104 +43,108 @@ public class Product implements Serializable {
 
     @Override
     public String toString() {
-        return "Product{" +
-                "proID='" + proID + '\'' +
-                ", proName='" + proName + '\'' +
-                ", price=" + price +
+        return "Sản phẩm{" +
+                "ID sản phẩm ='" + proID + '\'' +
+                ", Tên sản phẩm ='" + proName + '\'' +
+                ", Gía cả =" + price +
                 '}';
     }
 }
  class ProductProcess {
-    public static boolean writeObjectData(Product objProduct, String fileName) {
-        try {
-            // Tạo đối tượng ObjectOutputStream để ghi đối tượng vào file
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(fileName));
-            // Ghi đối tượng vào file
-            objectOutputStream.writeObject(objProduct);
-            // Đóng ObjectOutputStream
-            objectOutputStream.close();
-            // Trả về true nếu ghi thành công
-            return true;
-        } catch (IOException e) {
-            // Nếu có lỗi, in ra thông báo lỗi và trả về false
-            System.out.println("Lỗi khi ghi đối tượng vào file: " + e.getMessage());
-            return false;
-        }
-    }
+     public static boolean writeObjectData(Product objProduct, String fileName) {
+         try {
+             // Tạo đối tượng ObjectOutputStream để ghi đối tượng vào file
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("data.dat"));
+             // Ghi đối tượng vào file
+             objectOutputStream.writeObject(objProduct);
+             // Đóng ObjectOutputStream
+             objectOutputStream.close();
+             // Trả về true nếu ghi thành công
+             return true;
+         } catch (IOException e) {
+             // Nếu có lỗi, in ra thông báo lỗi và trả về false
+             System.out.println("Lỗi khi ghi đối tượng vào file: " + e.getMessage());
+             return false;
+         }
+     }
 
-    public static Product[] readObjectData(String fileName) {
-        try {
-            // Tạo đối tượng ObjectInputStream để đọc đối tượng từ file
-            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(fileName));
-            // Tạo một ArrayList để lưu các đối tượng đọc được từ file
-            ArrayList<Product> productList = new ArrayList<>();
-            // Đọc các đối tượng từ file cho đến khi kết thúc file
-            while (true) {
-                try {
-                    Product product = (Product) objectInputStream.readObject();
-                    productList.add(product);
-                } catch (EOFException e) {
-                    // Kết thúc file, thoát khỏi vòng lặp
-                    break;
-                }
-            }
-            // Đóng ObjectInputStream
-            objectInputStream.close();
-            // Nếu có ít nhất một đối tượng được đọc thành công từ file, trả về mảng đối tượng
-            if (productList.size() > 0) {
-                return productList.toArray(new Product[0]);
-            } else {
-                // Ngược lại, trả về null
-                return null;
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            // Nếu có lỗi, in ra thông báo lỗi và trả về null
-            System.out.println("Lỗi khi đọc đối tượng từ file: " + e.getMessage());
-            return null;
-        }
-    }
-}
-//     Lớp ProductProcess có hai phương thức để ghi và đọc đối tượng từ file. Phương thức writeObjectData ghi một đối tượng Product vào file và trả về true nếu việc ghi thành công, ngược lại trả về false. Phương thức readObjectData đọc một đối tượng từ file và trả về một mảng đối tượng Product. Nếu mảng đối tượng là null tức là file không có dữ liệu hoặc có lỗi.
- class ProductApp {
-    public static void main(String[] args) {
-        // Tạo đối tượng objProduct1 với phương thức khởi tạo không tham số
-        Product objProduct1 = new Product();
-        // Thiết đặt giá trị cho các thuộc tính của đối tượng objProduct1 bằng phương thức setter
-        objProduct1.setProID("P001");
-        objProduct1.setProName("Product 1");
-        objProduct1.setPrice(10000);
+     public static Product[] readObjectData(String fileName) {
+         try {
+             // Tạo đối tượng ObjectInputStream để đọc đối tượng từ file
+             ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("data.dat"));
+             // Tạo một ArrayList để lưu các đối tượng đọc được từ file
+             ArrayList<Product> productList = new ArrayList<>();
+             // Đọc các đối tượng từ file cho đến khi kết thúc file
+             while (true) {
+                 try {
+                     Product product = (Product) objectInputStream.readObject();
+                     productList.add(product);
+                 } catch (EOFException e) {
+                     // Kết thúc file, thoát khỏi vòng lặp
+                     break;
+                 }
+             }
+             // Đóng ObjectInputStream
+             objectInputStream.close();
+             // Nếu có ít nhất một đối tượng được đọc thành công từ file, trả về mảng đối tượng
+             if (productList.size() > 0) {
+                 return productList.toArray(new Product[0]);
+             } else {
+                 // Ngược lại, trả về null
+                 return null;
+             }
+         } catch (IOException | ClassNotFoundException e) {
+             // Nếu có lỗi, in ra thông báo lỗi và trả về null
+             System.out.println("Lỗi khi đọc đối tượng từ file: " + e.getMessage());
+             return null;
+         }
 
-        // Tạo đối tượng objProduct2 với phương thức khởi tạo 3 tham số
-        Product objProduct2 = new Product("P002", "Product 2", 20000);
+     }
+ }
 
-        // Đặt tên file là "object.dat"
-        String fileName = "object.dat";
 
-        // Ghi đối tượng objProduct1 vào file
-        if (ProductProcess.writeObjectData(objProduct1, fileName)) {
-            System.out.println("Ghi đối tượng objProduct1 vào file thành công");
-        } else {
-            System.out.println("Ghi đối tượng objProduct1 vào file không thành công");
-        }
+     //     Lớp ProductProcess có hai phương thức để ghi và đọc đối tượng từ file. Phương thức writeObjectData ghi một đối tượng Product vào file và trả về true nếu việc ghi thành công, ngược lại trả về false. Phương thức readObjectData đọc một đối tượng từ file và trả về một mảng đối tượng Product. Nếu mảng đối tượng là null tức là file không có dữ liệu hoặc có lỗi.
+    class ProductApp {
+         public static void main(String[] args) {
+             // Tạo đối tượng objProduct1 với phương thức khởi tạo không tham số
+             Product objProduct1 = new Product("P001", "Laptop Dell Inspiron 15", 15000000, "Dell");
+             // Thiết đặt giá trị cho các thuộc tính của đối tượng objProduct1 bằng phương thức setter
+             objProduct1.setProID("P001");
+             objProduct1.setProName("Product 1");
+             objProduct1.setPrice(10000);
 
-        // Ghi đối tượng objProduct2 vào file
-        if (ProductProcess.writeObjectData(objProduct2, fileName)) {
-            System.out.println("Ghi đối tượng objProduct2 vào file thành công");
-        } else {
-            System.out.println("Ghi đối tượng objProduct2 vào file không thành công");
-        }
+             // Tạo đối tượng objProduct2 với phương thức khởi tạo 3 tham số
+             Product objProduct2 = new Product("P002", "Product 2", 20000);
 
-        // Đọc các đối tượng từ file và lưu vào mảng đối tượng
-        Product[] products = ProductProcess.readObjectData(fileName);
+             // Đặt tên file là "object.dat"
+             String fileName = "data.dat";
 
-        // Nếu mảng đối tượng không phải là null, hiển thị thông tin các đối tượng
-        if (products != null) {
-            for (Product product : products) {
-                System.out.println(product.toString());
-            }
-        } else {
-            System.out.println("File không có dữ liệu hoặc có lỗi");
-        }
-    }
-}
+             // Ghi đối tượng objProduct1 vào file
+             if (ProductProcess.writeObjectData(objProduct1, fileName)) {
+                 System.out.println("Ghi đối tượng objProduct1 vào file thành công");
+             } else {
+                 System.out.println("Ghi đối tượng objProduct1 vào file không thành công");
+             }
+
+             // Ghi đối tượng objProduct2 vào file
+             if (ProductProcess.writeObjectData(objProduct2, fileName)) {
+                 System.out.println("Ghi đối tượng objProduct2 vào file thành công");
+             } else {
+                 System.out.println("Ghi đối tượng objProduct2 vào file không thành công");
+             }
+
+             // Đọc các đối tượng từ file và lưu vào mảng đối tượng
+             Product[] products = ProductProcess.readObjectData("data.dat");
+
+             // Nếu mảng đối tượng không phải là null, hiển thị thông tin các đối tượng
+             if (products != null) {
+                 for (Product product : products) {
+                     System.out.println(product.toString());
+                 }
+             } else {
+                 System.out.println("File không có dữ liệu hoặc có lỗi");
+             }
+         }
+     }
+
 //    Lớp ProductApp là lớp thực thi chứa hàm main(). Trong hàm main(), ta tạo hai đối tượng Product và ghi chúng vào file bằng phương thức writeObjectData của lớp ProductProcess. Sau đó, ta đọc các đối tượng từ file bằng phương thức readObjectData của lớp ProductProcess và hiển thị thông tin các đối tượng đọc được.
--- src/bai5/TH1.java
+++ src/bai5/TH1.java
@@ -1,41 +1,48 @@
 package bai5;
-import java.io.*;
-public class TH2 {
 
-            public static void main(String args[]) {
-                try {
+import java.io.*;
+import java.util.Scanner;
+
+public class TH1 {
+    public static void main(String args[]) {
+        try {
 //                     Tạo file person.dat
-                    File file = new File("C:\\Users\\Dell\\IdeaProjects\\OOP2\\src\\bai5\\person.dat");
+            File file = new File("src/bai5/person.dat");
 
 //                   Tạo đối tượng FileOutputStream để ghi dữ liệu vào file đặt tên là person.dat
-                    FileOutputStream fos = new FileOutputStream(file);
+            FileOutputStream fos = new FileOutputStream(file);
 
 //                     Nhập vào một chuỗi bất kỳ
-                    String str = "Hello World!";
+
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Nhập vào :");
+            String str=sc.nextLine();
 
 //                  Ghi chuỗi nhập trên vào file
-                    fos.write(str.getBytes());
+            fos.write(str.getBytes());
 
 //                   Mở file kiểm tra dữ liệu trong file
-                    System.out.println("Nội dung file sau khi ghi:");
-                    FileInputStream fis = new FileInputStream(file);
+            System.out.println("Nội dung file sau khi ghi:");
+            FileInputStream fis = new FileInputStream(file);
 
 //                     Đọc dữ liệu sử dụng phương thức read()
-                    int content;
-                    while ((content = fis.read()) != -1) {
-                        // convert to char and display
-                        System.out.print((char) content);
-                    }
-
-                    //  Đóng  FileInputStream/ FileOutputStream sau khi thực hiện đọc dữ liệu xong bằng việc sử dụng phương thức close()
-                    fis.close();
-                    fos.close();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            int a;
+            while ((a = fis.read()) != -1) {
+                System.out.print((char) a);
             }
+//    sử dụng một vòng lặp while để đọc dữ liệu từ đối tượng FileInputStream và sau đó chuyển đổi từng byte của dữ liệu đó thành ký tự và hiển thị trên màn hình console.
+
+// Cụ thể, giá trị trả về của phương thức fis.read() là một byte của dữ liệu được đọc từ file. Trong khi giá trị trả về khác -1 (tức là chưa đọc hết file), chúng ta chuyển đổi byte đó thành ký tự bằng cách sử dụng ép kiểu (char) content và hiển thị ký tự đó trên màn hình console bằng phương thức System.out.print(). Khi đọc hết file, phương thức fis.read() sẽ trả về giá trị -1, và vòng lặp sẽ kết thúc.
+
+            //  Đóng  FileInputStream/ FileOutputStream sau khi thực hiện đọc dữ liệu xong bằng việc sử dụng phương thức close()
+            fis.close();
+            fos.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+}
 
 
 
